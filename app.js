cde3cfdfc2555ed0d664df66e41ad056ce811582
matao <tma@netgen.com.cn>
2012-03-21 14:55:24 +0800

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var shortly = require('./lib/shortly');
+var store = require('./lib/store');
+
+var defaultUrl = 'http://www.gupiao213.cn/';
+var baseUrl = 'http://l.gupiao123.cn/';
+
+var app = express.createServer();
+
+app.get('/', function(req, res){
+  res.redirect( defaultUrl );
+});
+
+app.get(/^\/([a-zA-Z0-9]{6})$/, function(req, res){
+  var longUrl = store.findUrl( req.params[0] ) || defaultUrl;
+  res.redirect( longUrl );
+});
+
+app.get('/v1/shorten', function(req, res){
+  var hash = store.findHash( req.query.longUrl, shortly.hasher );
+  
+  var ret = {
+    "status_code": 200,
+    "data": {
+      "url": baseUrl + hash,
+      "hash": hash,
+      "long_url": req.query.longUrl
+    }, 
+    "status_txt": "OK"
+  };
+  res.send( JSON.stringify( ret, null, '  ' ) );
+});
+
+app.listen(3001);
